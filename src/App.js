3f84cd2ac06fffba43e53ea9cdbd21668235e23b
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-28 01:32:02 +0700

    Skills Section & Slider sắp xếp lại bố cục

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
-import NavBar from './components/NavBar';
-import Banner from './components/Banner';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './components/Navbar/NavBar';
+import Banner from './components/Banner/Banner';
+import Skills from './components/Skills/Skills';
 function App() {
   return (
     <div className="App">
         <NavBar />
         <Banner />
+        <Skills />
     </div>
   );
 }
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
-import headerImg from "../assets/img/header-img.svg"
+import headerImg from '../../assets/img/header-img.svg'
+
+import './Banner.scss'
 
 function Banner() {
     const [loopNum, setLoopNum] = useState(0);
