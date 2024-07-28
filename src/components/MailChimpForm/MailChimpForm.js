ad4355d2d27fefdc3c1bb2766603e85de351e8df
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-28 20:25:16 +0700

    Newsletter Subscribe

---
+++ src/components/MailChimpForm/MailChimpForm.js
@@ -0,0 +1,25 @@
+import MailchimpSubscribe from "react-mailchimp-subscribe"
+import NewsLetter from "../Newsletter/Newsletter";
+function MailChimpForm() {
+    const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=
+        ${process.env.REACT_APP_MAILCHIMP_U}&id=
+        ${process.env.REACT_APP_MAILCHIMP_ID}`;
+    return ( 
+        <>
+            <MailchimpSubscribe 
+                url={postUrl}
+                render={({subscribe, status, message}) => {
+                    <NewsLetter
+                        status={status}
+                        message={message}
+                        onValidated={FormData => subscribe(FormData)}
+                    >
+                        
+                    </NewsLetter>
+                }}
+            />
+        </>
+     );
+}
+
+export default MailChimpForm;
